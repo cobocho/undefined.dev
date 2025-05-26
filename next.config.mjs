f953e44d06e07f5e3568b347b7dd0838c23c2f52
Kim Min-gyu <99083803+cobocho@users.noreply.github.com>
2025-05-26 22:30:52 +0900

    chore: `next.config.js` 확장자 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const path = require('path')
+import { fileURLToPath } from 'url'
+import path, { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
